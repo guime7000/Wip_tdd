860702ce53bdda9192cbda4a72ed2c46ea70257f
guime7000 <tguimezanes@gmail.com>
2023-02-10 09:26:55 +0100

    multiple of 7 OK

--- katabigbang.py
+++ katabigbang.py
@@ -1,8 +1,11 @@
 def bigbang(inInt : int) -> str :
     """Return a specific string given conditions on the inInt integer parameter"""
     outList = []
+
     if inInt%5 == 0:
         outList.append("big")
     
-
+    if inInt%7 == 0:
+        outList.append("bang")
+        
     return "".join(outList)
