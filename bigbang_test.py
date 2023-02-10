6176d2c6453e97d2551c3aecfff64e14723f242c
guime7000 <tguimezanes@gmail.com>
2023-02-10 09:33:33 +0100

    multiple of 5 AND 7 AND 11 OK

--- bigbang_test.py
+++ bigbang_test.py
@@ -12,3 +12,10 @@ def test_bigbang_multiple_of_33()-> None:
     """Tests if a multiple of 11 is returned as big"""
     assert bigbang(33) == "boom"    
 
+def test_bigbang_multiple_of_5_7_11() -> None :
+    """Tests if a multiple of 5 AND 7 AND 11 returns bigbangboom"""
+    assert bigbang(385) == "bigbangboom"
+
+def test_bigbang_multiple_of_5_2() -> None:
+    """ Tests if a multiple of 5 AND 2 returns big"""
+    assert bigbang(10) == "big"
