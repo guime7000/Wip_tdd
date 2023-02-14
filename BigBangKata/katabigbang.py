d8aa7c15b213b548c3d12e5948852769659e39fc
guime7000 <tguimezanes@gmail.com>
2023-02-14 13:25:32 +0100

    modified code using extract method

--- BigBangKata/katabigbang.py
+++ BigBangKata/katabigbang.py
@@ -2,11 +2,43 @@ def is_multiple_of(inInt: int, divisor: int) -> bool :
     """
     Returns true if inInt is a multiple of divisor, False otherwise
     """
-    isMultiple = False
-    if inInt % divisor == 0 :
-        isMultiple = True
+    return inInt % divisor == 0
+
+def divisor_mapping() -> dict :
+    """
+    Returns a dict containing pairs of divisor / corresponding word
+
+    Modify outDict as you wish to fit your needs in bigbang function :
+    each key / value pair of the dict corresponds to an int value as key (divisor) and a string as value 
+    """
+    outDict = {5 : "big",
+                7 : "bang",
+                11 : "boom"}
+
+    return outDict
+
+def create_word_list(inInt: int, inDict : dict) -> list :
+    """
+    Checks if an inInt integer value is a multiple of some divisors.
+    If so, appends a specific word to a list the fucntion will return
+    """
+    outList = []
+    for key in inDict.keys():
+        if is_multiple_of(inInt, key):
+            outList.append(inDict[key])
+
+    return outList
+
+def reverse_word_list(inInt: int, divisor: int, inList: list) -> list :
+    """
+    Returns a reversed inList if inInt is a multiple of divisor
+    Returns inList otherwise
+    """
+    if is_multiple_of(inInt, divisor):
+        inList.reverse()
     
-    return isMultiple
+    return inList
+
 
 def bigbang(inInt : int) -> str :
     """
@@ -14,22 +46,14 @@ def bigbang(inInt : int) -> str :
     """
 
     # Dictionnary to map a divisor and the output word associated to it
-    # dict item -> divisor : associated_word
-    divisorMapping = {5 : "big",
-                    7 : "bang",
-                    11 : "boom"}
-    outList = []
+    divisorMapping = divisor_mapping()
 
-    for key in divisorMapping.keys():
-        if is_multiple_of(inInt, key):
-            outList.append(divisorMapping[key])
+    outList = create_word_list(inInt, divisorMapping)
+
+    outList = reverse_word_list(inInt, 2, outList)
 
     if len(outList) == 0 : 
         # inInt is not a multiple of 5, 7 or 11
         outList.append(str(inInt))
-
-    if is_multiple_of(inInt, 2):
-        #inInt is even
-        outList.reverse()
 
     return "".join(outList)
