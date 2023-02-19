5eadb43204595f85460f0d54694e9a543c545e76
guime7000 <tguimezanes@gmail.com>
2023-02-19 16:59:59 +0100

    possible end of stage 1 and 2

--- StringCalculator/Stage1_2/stringcalculator_stage1_2.py
+++ StringCalculator/Stage1_2/stringcalculator_stage1_2.py
@@ -1,22 +1,29 @@
 def parse_expression(expression : str, delimiter: str = ",") -> list :
     """
-    Parses a string expression containing integers separated by a delimiter
-    to return a list of those integers
+    Parses an expression containing integers separated by a delimiter
+    to return a list where delimiters have been removed
     """
-    print(delimiter)
-    pass
+    return expression.split(delimiter)
+
+def converts_to_integer(stringList : list) -> list :
+    """
+    Converts a list of integers given as strings to a list containing integers
+    """
+    convertedList = []
+    for element in stringList :
+        convertedList.append(int(element))
+    return convertedList
 
 def calculate(expression: str) -> int:
     """
     Returns the sum of integers written in expression
     """
-    calculatedSum = 0
 
     if len(expression) == 0 :
-        return calculatedSum
+        return 0
     
     else :
-        parse_expression(expression)
+        parsedExpressions = parse_expression(expression)
+        integerList = converts_to_integer(parsedExpressions)
 
-    return calculatedSum
-
+    return sum(integerList)
--- StringCalculator/Stage1_2/stringcalculator_stage1_2_test.py
+++ StringCalculator/Stage1_2/stringcalculator_stage1_2_test.py
@@ -1,14 +1,22 @@
-from stringcalculator_stage1_2 import calculate, parse_expression
+from stringcalculator_stage1_2 import calculate, parse_expression, converts_to_integer
+
+##################### parse_expression #######################
 
 def test_parse_expression_empty_In() -> None:
-    assert parse_expression(" ") == []
+    assert parse_expression("") == ['']
 
 def test_parse_expression_1_arg() -> None:
-    assert parse_expression("1") == [1]
+    assert parse_expression("1") == ['1']
 
-def test_parse_expression_1_arg() -> None:
-    assert parse_expression("1,2") == [1,2]
+def test_parse_expression_2_arg() -> None:
+    assert parse_expression("1,2") == ['1','2']
 
+################# converts_to_integer# #############################
+
+def test_converts_to_integer() -> None :
+    assert converts_to_integer(['1','2','3']) == [1,2,3]
+
+################ calculate ####################################
 def test_calculate_empty_string() -> None:
     """ If an empty string is given as parameter, should return 0 """
     assert calculate("") == 0 
@@ -27,8 +35,8 @@ def test_calculate_2_comma_sep_Args() -> None:
 
 def test_calculate_3_comma_sep_Args() -> None:
     """ 3 comma separated args as entry ("1,2,3"), returns 6 (= 1 + 2 + 3)"""
-    assert calculate("1,4") == 6
+    assert calculate("1,2,3") == 6
 
 def test_calculate_5_comma_sep_Args() -> None:
     """ 5 comma separated args as entry ("1,2,3,4,5"), returns 15 (= 1 + 2 + 3 + 4 + 5)"""
-    assert calculate("1,4") == 15
+    assert calculate("1,2,3,4,5") == 15
