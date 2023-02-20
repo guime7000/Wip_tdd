11d4dd658360b780363bafd838926c4af4271ccd
guime7000 <tguimezanes@gmail.com>
2023-02-20 21:17:05 +0100

    TPP on Air 7

--- StringCalculator/Stage0/stringcalculator_stage0.py
+++ StringCalculator/Stage0/stringcalculator_stage0.py
@@ -3,20 +3,26 @@ def calculate(expression: str) -> int:
     Returns the sum of integers written in expression
     """
     calculatedSum = 0
+    integerList = []
 
     if len(expression) == 0:
         return 0
 
     else:
-        if expression[-1] != ",":
-            calculatedSum += int(expression)
-            return calculatedSum
+        # if expression[-1] != ",":
+        #     calculatedSum += int(expression)
+        #     return calculatedSum
 
+        expressionLength = len(expression)
         temp_stringed_int = ""
-        for digit in expression:
+        for idx in range(expressionLength):
+            digit = expression[idx]
             temp_stringed_int += digit
-            if digit == ",":
-                calculatedSum += int(temp_stringed_int)
+            if digit == "," or idx == expressionLength - 1:
+                integerList.append(temp_stringed_int)
+                integerList.append(digit)
                 temp_stringed_int = ""
 
+        for idx in range(0, len(integerList), 2):
+            calculatedSum += int(integerList[idx])
     return calculatedSum
