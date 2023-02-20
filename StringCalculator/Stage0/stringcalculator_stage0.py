4eaa7ef684bc56afed7d81ed011d9bdc6c85ce00
guime7000 <tguimezanes@gmail.com>
2023-02-20 21:30:25 +0100

    From now on, I'm refactoring

--- StringCalculator/Stage0/stringcalculator_stage0.py
+++ StringCalculator/Stage0/stringcalculator_stage0.py
@@ -3,16 +3,12 @@ def calculate(expression: str) -> int:
     Returns the sum of integers written in expression
     """
     calculatedSum = 0
-    integerList = []
+    expressionSplit = []
 
     if len(expression) == 0:
         return 0
 
     else:
-        # if expression[-1] != ",":
-        #     calculatedSum += int(expression)
-        #     return calculatedSum
-
         expressionLength = len(expression)
         temp_stringed_int = ""
         for idx in range(expressionLength):
@@ -20,10 +16,10 @@ def calculate(expression: str) -> int:
             if digit != ",":
                 temp_stringed_int += digit
             if digit == "," or idx == expressionLength - 1:
-                integerList.append(temp_stringed_int)
-                integerList.append(digit)
+                expressionSplit.append(temp_stringed_int)
+                expressionSplit.append(digit)
                 temp_stringed_int = ""
 
-        for idx in range(0, len(integerList), 2):
-            calculatedSum += int(integerList[idx])
+        for idx in range(0, len(expressionSplit), 2):
+            calculatedSum += int(expressionSplit[idx])
     return calculatedSum
