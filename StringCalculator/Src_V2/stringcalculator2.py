ce4876a5bd755a85425c4784618573358df6f16e
guime7000 <tguimezanes@gmail.com>
2023-02-23 00:06:48 +0100

    Going forward with 3 numbers

--- StringCalculator/Src_V2/stringcalculator2.py
+++ StringCalculator/Src_V2/stringcalculator2.py
@@ -1,6 +1,31 @@
 def calculate(expression: str) -> int:
+    if len(expression) == 0:
+        return 0
+
+    delimiterIndex = []
     for i in range(len(expression)):
         if expression[i] == ",":
-            return int(expression[:i]) + int(expression[i + 1 :])
+            delimiterIndex.append(i)
 
-    return int(expression)
+    if len(delimiterIndex) == 0:
+        totalSum = int(expression)
+    else:
+        totalSum = int(expression[: delimiterIndex[0]])
+
+        if len(delimiterIndex) == 1:
+            i = 0
+            totalSum = int(expression[: delimiterIndex[i]])
+            totalSum = totalSum + int(expression[delimiterIndex[i] + 1 :])
+
+        if len(delimiterIndex) == 2:
+            i = 0
+            totalSum = int(expression[: delimiterIndex[i]])
+            totalSum = totalSum + int(
+                expression[delimiterIndex[i] + 1 : delimiterIndex[i + 1]]
+            )
+            i = 1
+            totalSum = totalSum + int(expression[delimiterIndex[i] + 1 :])
+
+    return totalSum
+
+    # return int(expression)
--- StringCalculator/Src_V2/stringcalculator2_test.py
+++ StringCalculator/Src_V2/stringcalculator2_test.py
@@ -1,6 +1,10 @@
 from stringcalculator2 import calculate
 
 
+def test_calculate_empty_string() -> None:
+    assert calculate("") == 0
+
+
 def test_calculate_1_nb() -> None:
     assert calculate("0") == 0
     assert calculate("2") == 2
@@ -11,3 +15,8 @@ def test_calculate_2_nb() -> None:
     assert calculate("2,3") == 5
     assert calculate("20,30") == 50
     assert calculate("70,123") == 193
+
+
+def test_calculate_3_nb() -> None:
+    assert calculate("1,2,4") == 7
+    assert calculate("10,20,40") == 70
